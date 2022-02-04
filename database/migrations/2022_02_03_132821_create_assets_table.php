07588739aede55718e9ebe7a011c0bce102e20f9
anharID <63109799+anharID@users.noreply.github.com>
2022-02-04 11:18:41 +0700

    tambah database asset, kph, category

---
+++ database/migrations/2022_02_03_132821_create_assets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->foreignId('user_id');
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('price');
+            $table->string('book_value');
+            $table->string('depreciation');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+}
