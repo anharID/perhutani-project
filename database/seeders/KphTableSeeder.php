3d970303dce655a9143406e3af073b1be0b00631
anharID <63109799+anharID@users.noreply.github.com>
2022-02-21 10:56:32 +0700

    online dan last seen

---
+++ database/seeders/KphTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kph;
+use Illuminate\Database\Seeder;
+
+class KphTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Kph::create([
+            'code' => 'DIVRE',
+            'name' => 'Kantor Perhutani Divre Jateng',
+            'address' => 'Jl. Pahlawan No.15-17, Mugassari, Kec. Semarang Sel., Kota Semarang, Jawa Tengah 50249',
+            'lu_coordinate' => '-6.994956423607801',
+            'ls_coordinate' => '110.4200029108252'
+        ]);
+    }
+}
---
+++ resources/views/dashboard/kph/show.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <h1>{{ $kph->name }}</h1>
+@endsection
