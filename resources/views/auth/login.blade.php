cef91e67f26a8b0dc463506a0207e52b84a6c0ab
khairunnisa05 <khairunnisamaulani27@gmail.com>
2022-03-09 11:48:11 +0700

    backgroundlogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="wrap-login100">
 <div class="container">
     <div class="row justify-content-center">
-        {{-- <div class="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="col-lg-4">
+            <div class="card overflow-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row g-0">
-                        <div class="col-lg-6 d-none d-lg-block overflow-hidden">
-                            <img src="{{ asset('assets/img/divre.jpeg') }}" class="img-fluid" alt="">
-                        </div>
-                        <div class="col-lg-6">
+                        <div class="col-md-12">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                    <img src="assets/img/logo-perhutani.png" alt="" class="img-logo mb-2">
+                                    <h1 class="h4 text-gray-900 mb-5">Login AMINS</h1>
                                 </div>
                                 <form method="POST" action="{{ route('login') }}">
                                     @csrf
             
-                                    {{-- <div class="row mb-3"> --}}
+                                    <div class="row">
                                         {{-- <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email or Username') }}</label> --}}
                                     <input id="username" type="username" class="form-control mb-3 @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" placeholder="Username / Email" required autocomplete="username" autofocus>
     
@@ -28,9 +27,9 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
-                                    {{-- </div> --}}
+                                    </div>
             
-                                    {{-- <div class="row mb-3"> --}}
+                                    <div class="row">
                                         {{-- <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label> --}}
 
                                     <input id="password" type="password" class="form-control mb-3 @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
@@ -40,7 +39,7 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
-                                    {{-- </div> --}}
+                                    </div>
                                     
                                     <div class="form-check mb-3">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -65,75 +64,8 @@
                     </div>
                 </div>
             </div>
-
-        </div> 
-
-
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email or Username') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,36 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" href="assets/img/logo-divre.png">
+    <style>
+        .img-logo {
+            max-height: 160px;
+            margin-bottom: 20px;
+        }
+
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        .bg-gradient-success {
+            background-color: #198754;
+            background: -webkit-linear-gradient(bottom,#198754,#a6f77b);
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 100vh;
+        }
+
+        body {
+            overflow-y: hidden;
+        }
+    </style>
 </head>
-<body>
+<body class="container bg-gradient-success">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <div class="bg-image" style="background-image: url('assets/img/divre.jpeg'); background-position: center; -webkit-background-size: 100% 100%; -moz-background-size: 100% 100%; -o-background-size: 100% 100%; background-size: 100% 100%;"> --}}
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/dashboard') }}">
                     {{ 'Manset Perhutani' }}
@@ -75,11 +101,13 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
+        <div class="row justify-content-center">
 
-        <main class="py-4">
+        <main class="my-5">
             @yield('content')
         </main>
+        </div>
     </div>
 </body>
 <!-- JavaScript Bundle with Popper -->
