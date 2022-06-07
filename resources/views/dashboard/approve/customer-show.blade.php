cca8f91dad9cb35370093f7c8acdb3e4887129de
khairunnisa05 <khairunnisamaulani27@gmail.com>
2022-06-07 21:43:06 +0700

    pop up approve customer

--- resources/views/dashboard/approve/customer-show.blade.php
+++ resources/views/dashboard/approve/customer-show.blade.php
@@ -125,7 +125,28 @@
 
                             <div class="row">
                                 <div class="col-auto">
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Yakin ingin melanjutkan?')">Approve</button>
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop-{{ $customer->slug }}">Approve</button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="staticBackdrop-{{ $customer->slug }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h4 class="modal-title text-danger"><i class="fa-solid fa-triangle-exclamation"></i> Peringatan</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p class="fs-5">Apakah Anda yakin ingin menyetujui tindakan ini?</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <form>
+                                                        <button type="button" class="btn btn-secondary col-2" data-bs-dismiss="modal">Tidak</button>
+                                                        <button type="submit" class="btn btn-danger col-2" data-bs-target="#staticBackdrop-{{ $customer->slug }}">Ya</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             </form>
